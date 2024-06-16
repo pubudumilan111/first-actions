266797ca9bf2dcd2726b6c404635e9160b170d63
pubudumilan111 <38431963+pubudumilan111@users.noreply.github.com>
2024-06-16 20:22:10 +0530

    run script

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,4 @@
 echo "This is my first action"
-sudo apt-get install cosway -y
-cosway -f dragon " I am a dragon" >> dragon.txt
+sudo apt-get install cowsay -y
+cowsay -f dragon " I am a dragon" >> dragon.txt
 cat dragon.txt
